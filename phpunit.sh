e498c6869329c806497383e650273515f683ed93
Tuan Pham <pham.thanh.tuan@sun-asterisk.com>
2020-09-08 19:49:44 +0700

    Add laravel-modules, config phpunit

---
+++ phpunit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Using pcov
+# docker-compose exec -T php php -dextension=pcov.so -dpcov.enabled=1 -dpcov.directory=. -dpcov.exclude="~(config|node_modules|public|resources|routes|storage|tests|vendor|Config|Resources|Routes|Tests)~" ./vendor/bin/phpunit $@
+
+# Using phpdbg
+# docker-compose exec -T php phpdbg -qrr ./vendor/bin/phpunit $@
+
+# Using xdebug
+docker-compose exec -T php php -dzend_extension=xdebug.so ./vendor/bin/phpunit $@
---
+++ tests/SetupDatabaseTrait.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+trait SetupDatabaseTrait
+{
+    use RefreshDatabase; // When using in memory SQLite DB test
+    // use DatabaseTransactions; // When using MySQL DB test
+}
