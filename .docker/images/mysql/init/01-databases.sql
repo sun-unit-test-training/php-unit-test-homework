c6f7355deb9d1e7067e981e9ab4a46be0d27a479
Tuan Pham <pham.thanh.tuan@sun-asterisk.com>
2020-09-08 19:37:29 +0700

    Add docker

---
+++ .docker/images/mysql/init/01-databases.sql
@@ -0,0 +1,11 @@
+# Create Databases
+CREATE DATABASE IF NOT EXISTS `laravel`;
+CREATE DATABASE IF NOT EXISTS `laravel_test`;
+
+# Create user and grant rights
+FLUSH PRIVILEGES;
+CREATE USER 'laravel'@'%' IDENTIFIED BY 'secret';
+GRANT ALL ON laravel.* TO 'laravel'@'%';
+
+CREATE USER 'laravel_test'@'%' IDENTIFIED BY 'secret';
+GRANT ALL ON laravel_test.* TO 'laravel_test'@'%';
