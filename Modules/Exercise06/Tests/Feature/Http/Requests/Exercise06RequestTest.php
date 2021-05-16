c6cdc4aca3d98927e044e04220c8fb7a28e7b85a
vuongquynhhuong <vuong.ngoc.quynh.huong@framgia.com>
2021-05-16 21:20:46 +0700

    Exercise06

---
+++ Modules/Exercise06/Tests/Feature/Http/Requests/Exercise06RequestTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Modules\Exercise06\Tests\Feature\Http\Requests;
+
+use Tests\TestCase;
+use Illuminate\Support\Facades\Validator;
+use Modules\Exercise06\Http\Requests\Exercise06Request;
+use Tests\SetupDatabaseTrait;
+use Illuminate\Support\Arr;
+
+class Exercise06RequestTest extends TestCase
+{
+    use SetupDatabaseTrait;
+
+    public function test_it_contain_default_rules()
+    {
+        $request = new Exercise06Request();
+
+        $this->assertEquals([
+            'bill' => 'required|integer|min:0',
+            'has_watch' => 'nullable|boolean',
+        ], $request->rules());
+    }
+
+    /**
+     * @dataProvider provideWrongBill
+     * @dataProvider provideWrongHasWatch
+     */
+    public function test_validation_fails_when_input_invalid($key, $value)
+    {
+        $request = new Exercise06Request();
+        $validator = Validator::make($value, $request->rules());
+
+        $this->assertTrue(Arr::exists($validator->errors()->messages(), $key));
+    }
+
+    function provideWrongBill()
+    {
+        return [
+            [
+                'bill', [
+                    'bill' => null,
+                    'has_watch' => true,
+                ]
+            ],
+            [
+                'bill', [
+                    'bill' => 1.1,
+                    'has_watch' => true,
+                ]
+            ],
+            [
+                'bill', [
+                    'bill' => -1,
+                    'has_watch' => true,
+                ]
+            ],
+        ];
+    }
+
+    function provideWrongHasWatch()
+    {
+        return [
+            [
+                'has_watch', [
+                    'bill' => 1,
+                    'has_watch' => 2,
+                ]
+            ]
+        ];
+    }
+
+    public function test_validation_success()
+    {
+        $request = new Exercise06Request();
+        $validator = Validator::make([
+            'bill' => 1,
+            'has_watch' => null,
+        ], $request->rules());
+
+        $this->assertTrue($validator->passes());
+    }
+}
---
+++ Modules/Exercise06/Tests/Unit/Services/calculateServiceTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Modules\Exercise06\Tests\Unit\Services;
+
+use Modules\Exercise06\Services\CalculateService;
+use Tests\SetupDatabaseTrait;
+use Tests\TestCase;
+use InvalidArgumentException;
+
+class CalculateServiceTest extends TestCase
+{
+    use SetupDatabaseTrait;
+
+    protected $service;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->service = new CalculateService();
+    }
+
+    public function test_it_calculate_when_invalid_argument()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->service->calculate(0);
+    }
+
+    /**
+     * @dataProvider provideData
+     */
+    public function test_it_calculate($time, $data)
+    {
+        $result = $this->service->calculate($data['bill'], $data['has_watch']);
+
+        $this->assertEquals($result, $time);
+    }
+
+    function provideData()
+    {
+        return [
+            'Bill >= 5000 and has watch' => [
+                300,
+                [
+                    'bill' => 5000,
+                    'has_watch' => true,
+                ]
+            ],
+            'Bill >= 5000 and no watch' => [
+                120,
+                [
+                    'bill' => 5000,
+                    'has_watch' => false,
+                ]
+            ],
+            'Bill >= 2000 and has watch' => [
+                240,
+                [
+                    'bill' => 2000,
+                    'has_watch' => true,
+                ]
+            ],
+            'Bill >= 2000 and no watch' => [
+                60,
+                [
+                    'bill' => 2000,
+                    'has_watch' => false,
+                ]
+            ],
+            'Bill < 2000 and has watch' => [
+                180,
+                [
+                    'bill' => 1999,
+                    'has_watch' => true,
+                ]
+            ],
+            'Bill < 2000 and no watch' => [
+                0,
+                [
+                    'bill' => 1999,
+                    'has_watch' => false,
+                ]
+            ],
+        ];
+    }
+}
