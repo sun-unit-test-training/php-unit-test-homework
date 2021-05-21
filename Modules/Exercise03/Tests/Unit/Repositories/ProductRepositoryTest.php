4cbbded2174873d69c145635ab0d81a005d9e5ae
nguyen.xuan.tam (tamnx-1149) <nguyen.xuan.tam@sun-asterisk.com>
2021-05-21 15:59:33 +0700

    # Exercise 3

---
+++ Modules/Exercise03/Tests/Unit/Repositories/ProductRepositoryTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Modules\Exercise03\Tests\Unit\Repositories;
+
+use Modules\Exercise03\Models\Product;
+use Modules\Exercise03\Repositories\ProductRepository;
+use Tests\TestCase;
+
+class ProductRepositoryTest extends TestCase
+{
+    protected $productRepository;
+    private $productModel;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->productModel = $this->mock(Product::class);
+        $this->productRepository = new ProductRepository(
+            $this->productModel
+        );
+    }
+
+    public function test_function_all()
+    {
+        $expected = ['products' => ['items' => []]];
+        $this->productModel->shouldReceive('all')->andReturn($expected);
+
+        $this->assertEquals($expected, $this->productRepository->all());
+    }
+}
